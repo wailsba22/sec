d80276ab8b00285dd6e8163d221f4240c4c08b4c
wailsba22 <miryodsanger@gmail.com>
2026-05-05 15:32:08 +0100

    ads

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10967556
+    "zoneId": 10967643
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
